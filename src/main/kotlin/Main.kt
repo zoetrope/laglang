3cf49d2ea1167542e2e7b4247b817a04f6d5dce0
zoetrope <a.ikezoe@gmail.com>
2016-01-11 00:43:02 +0900

    add elastiko

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,11 +2,13 @@ import io.vertx.core.AbstractVerticle
 import io.vertx.core.logging.LoggerFactory
 import io.vertx.kotlin.lang.*
 import org.elasticsearch.common.settings.Settings;
+import elastiko.*
+import org.elasticsearch.common.unit.TimeValue
 
 class MainVerticle : AbstractVerticle() {
-    override fun start() {
-        val logger = LoggerFactory.getLogger("main")
+    val logger = LoggerFactory.getLogger("main")
 
+    override fun start() {
         val route = Route {
             apiRoute()
             staticFileRoute()
@@ -14,18 +16,32 @@ class MainVerticle : AbstractVerticle() {
         httpServer(8084, "0.0.0.0", block = route)
     }
 
-    private fun Route.apiRoute(){
+    private fun Route.apiRoute() {
 
-        val settings = Settings
+        val client = transportClient(listOf(address("localhost", 9300))) {
+            settings {
+                put("cluster.name", "elasticsearch")
+            }
+        }
 
         GET("/api/v1/conf") { request ->
-            bodyJson {
-                "field1" to "this is a test"
+
+            client.searchAsync("laglang-*") {
+                setQuery(termQuery("word", "test"))
+                setTimeout(TimeValue.timeValueSeconds(5))
+            } success {
+                bodyJson {
+                    it.hits.hits.map { it.source }
+                }
+            } fail {
+                bodyJson {
+                    "message" to it.message
+                }
             }
         }
     }
 
-    private fun Route.staticFileRoute(){
+    private fun Route.staticFileRoute() {
         GET(glob("/static/**/*")) { request -> sendFile(request.path().substring(1)) }
         GET("/") { request -> sendFile("static/templates/index.html") }
         GET("/index.html") { request -> sendFile("static/templates/index.html") }
